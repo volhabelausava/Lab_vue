b10bbf605e7554e687772fb644ae692f5f07d59e
Volha Belausava <volha.belausava@gmail.com>
2022-04-12 12:04:50 +0300

    Added static footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <Header />
-  <router-view />
+  <Header/>
+  <main class="main">
+    <router-view/>
+  </main>
+  <Footer/>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import Header from '@/components/Header.vue';
+import TheHeader from '@/components/TheHeader.vue';
+import TheFooter from '@/components/TheFooter.vue';
 
 @Options({
   components: {
-    Header
+    TheHeader,
+    TheFooter
   },
 })
 export default class App extends Vue {
@@ -18,14 +23,23 @@ export default class App extends Vue {
 </script>
 
 <style lang="scss">
-body {
+html, body {
   margin: 0;
+  height: 100%;
 }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.main {
+  flex: 1 1 auto;
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,54 @@
+<template>
+  <header class="header">
+    <h1>Games Store</h1>
+    <nav>
+      <router-link :to="{ name: 'Home' }">Home</router-link>
+      <router-link :to="{ name: 'Products' }">Products</router-link>
+      <router-link :to="{ name: 'About' }">About</router-link>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+  background-color: #121211;
+  color: #E2E2E2;
+
+  h1 {
+    font-size: 30px;
+    margin: 15px 20px;
+  }
+
+  nav {
+    display: flex;
+    align-items: stretch;
+
+    a {
+      display: flex;
+      align-items: center;
+      padding: 0 20px;
+      font-size: 18px;
+      text-decoration: none;
+      color: #E2E2E2;
+      border-bottom: solid 4px #121211;
+
+      &.router-link-exact-active, &:hover, &:focus {
+        background-color: #272727;
+        border-bottom: solid 4px #9D5DCD;
+      }
+
+      &:focus, &:hover {
+        outline: none;
+        color: #9D5DCD;
+      }
+
+      &.router-link-exact-active:focus {
+        color: #E2E2E2;
+      }
+    }
+  }
+}
+
+</style>
